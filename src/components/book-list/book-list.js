859103b8adfe67c79f97ca1eb9ef2d81cb5262a8
peterjj91 <peterjj91@gmail.com>
2019-02-14 23:57:35 +0200

    Async data, loading

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { withBookstoreService } from '../hoc';
 import { booksLoaded } from '../../actions';
 import { compose } from '../../utils';
+import Spinner from '../spinner';
 
 import './book-list.css';
 
@@ -12,14 +13,18 @@ class BookList extends Component {
   componentDidMount() {
     // receive data
     const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-    
-    // dispatch action to store
-    this.props.booksLoaded(data);
+    bookstoreService.getBooks()
+      .then((data) => {
+        // dispatch action to store
+        this.props.booksLoaded(data);
+      });
   };
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+    if (loading) {
+      return <Spinner />
+    }
     return (
       <ul className="book-list">
         {
@@ -35,8 +40,8 @@ class BookList extends Component {
 };
 
 // возвращает объект там где ключи - название свойств, которые мы назначили компоненту, а значение - значение которое мы просвоим
-const mapStateToProps = ({ books }) => {
-  return { books };
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
 };
 
 const mapDispatchToProps = {
