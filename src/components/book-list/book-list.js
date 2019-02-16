bc5a229be02400cf3121f6d8aea828b6041eeaee
peterjj91 <peterjj91@gmail.com>
2019-02-16 12:26:07 +0200

    Argument ownProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -12,16 +12,7 @@ import './book-list.css';
 
 class BookList extends Component {
   componentDidMount() {
-    // receive data
-    const { 
-      bookstoreService, 
-      booksLoaded, 
-      booksRequested, 
-      booksError } = this.props;
-    booksRequested();
-    bookstoreService.getBooks()
-      .then((data) =>  booksLoaded(data))
-      .catch((err) => booksError(err));
+    this.props.fetchBooks();
   };
 
   render() {
@@ -51,11 +42,18 @@ const mapStateToProps = ({ books, loading, error }) => {
   return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError
-}
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps;
+  
+  return {
+    fetchBooks: () => {
+      dispatch(booksRequested());
+      bookstoreService.getBooks()
+        .then((data) =>  dispatch(booksLoaded(data)))
+        .catch((err) => dispatch(booksError(err)));
+    }
+  }
+};
 
 // получаем книги из redux store 
 export default compose(
